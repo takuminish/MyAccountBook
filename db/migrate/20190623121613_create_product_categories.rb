c1f860f54667cf06e2256922d77a7ddecde234e3
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-06-23 21:19:06 +0900

    ProductStoreモデルの制約を追加

--- db/migrate/20190623121613_create_product_categories.rb
+++ db/migrate/20190623121613_create_product_categories.rb
@@ -1,8 +1,8 @@
 class CreateProductCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :product_categories do |t|
-      t.string :name
-      t.boolean :expense
+      t.string :name, null:false
+      t.boolean :expense, default: false
 
       t.timestamps
     end
