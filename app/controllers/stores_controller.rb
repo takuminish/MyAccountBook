134b1d43ddbcafe25753b3d5edfa29fcda3a4f89
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-06-24 19:16:38 +0900

    Storeモデルの登録に成功

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,20 @@
 class StoresController < ApplicationController
   def new
+
+    @store = Store.new
+
+  end
+
+  def create
+    
+    @store= Store.new(store_params)
+    @store.save
+
+  end
+
+  private def store_params
+
+    params.require(:store).permit(:name)
+
   end
 end
