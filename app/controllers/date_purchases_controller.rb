105bf5d098333d4949f75705ded9260fdf2568a0
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-06-25 20:29:56 +0900

    複数登録用のコントローラーを記述

--- app/controllers/date_purchases_controller.rb
+++ app/controllers/date_purchases_controller.rb
@@ -6,8 +6,20 @@ class DatePurchasesController < ApplicationController
   end
 
   def new
+    @date_purchase = DatePurchase.new
+    @date_purchase.product.build
   end
 
+  def create
+    @date_purchase = DatePurchase.new(date_purchase_params)
+    @date_purchase.save
+  end
   def edit
+  end
+
+
+  private def date_purchase_params
+  
+  params.require(:date_purchase).permit(:totalcost, :date, products_attributes:[:name, :price, :purchase_date, :store_id, :product_category_id])
   end
 end
