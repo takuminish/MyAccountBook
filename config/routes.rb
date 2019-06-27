d0977d41f69ecb8458a7990ac5d44e4e9a5642b9
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-06-27 21:19:38 +0900

    editのフォームを編集

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   resources :product_categories
   resources :stores
+  resources :products
   resources :date_purchases
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
