6f79e7872159d85939ed7fbf571c287541a48d1c
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-06-28 22:34:32 +0900

    permitする要素を追加

--- app/controllers/date_purchases_controller.rb
+++ app/controllers/date_purchases_controller.rb
@@ -46,7 +46,7 @@ class DatePurchasesController < ApplicationController
 
   private def date_purchase_params
   
-  params.require(:date_purchase).permit(:date, products_attributes:[:name, :price, :store_id, :product_category_id])
+  params.require(:date_purchase).permit(:date, store_purchases_attributes:[:store_id,products_attributes:[:name, :price, :product_category_id])
   end
 
   private def date_purchase_params_update
