79be1586f34af31d56a57b7a5c7e63424b9cd809
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-06-28 21:49:48 +0900

    destroyアクションの作成

--- app/controllers/store_purchase_controller_controller.rb
+++ app/controllers/store_purchase_controller_controller.rb
@@ -1,2 +1,8 @@
 class StorePurchaseControllerController < ApplicationController
+
+    def destroy
+        @store_purchase = StorePurchase.find(params[:id])
+        @store_purchase.destroy
+        redirect_back(fallback_location: stores_path)
+    end
 end
