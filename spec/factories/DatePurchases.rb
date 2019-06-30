a5a698af557a676740fd170524a764f9eab12a0d
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-06-30 15:20:14 +0900

    登録成功時のテストを記述

--- spec/factories/DatePurchases.rb
+++ spec/factories/DatePurchases.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
     factory :date_purchase do
         date {Faker::Date.backward}
+        total_cost {Faker::Number.number}
 
     end
 end
--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -4,10 +4,18 @@ RSpec.describe Product, type: :model do
     
     before do
         @product = build(:product)
+        date_purchase = create(:date_purchase)
+        store = create(:store)
+        store_purchase = create(:store_purchase, date_purchase_id: date_purchase.id, store_id: store.id)
+        product_category = create(:product_category)
+        @product.store_purchase_id = store_purchase.id
+        @product.product_category_id = product_category.id
     end
 
     describe 'バリデーションチェック' do
         it '全ての値が設定されており、外部キーもおっけーであれば登録' do
+
+            expect(@product).to be_valid
             
         end
     end
