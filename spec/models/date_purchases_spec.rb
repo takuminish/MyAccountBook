178f58855ee5ceed84ac8951722ccd356ac8c6f2
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-06-30 21:23:16 +0900

    登録成功のテストを記述

--- spec/models/date_purchases_spec.rb
+++ spec/models/date_purchases_spec.rb
@@ -3,7 +3,19 @@ require 'rails_helper'
 RSpec.describe DatePurchase, type: :model do
 
     before do
+
         @date_purchase = build(:product_category)
+    
     end
+
+    describe '登録成功' do
+
+        it 'バリデーションを満たしていれば登録成功' do
+
+        expect(@date_purchase).to be_valid
+
+        end
+    end
+
 
 end
