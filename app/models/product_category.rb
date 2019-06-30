34f85c9ace57e834ffcd68186eb4418287dd5931
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-06-30 22:01:28 +0900

    ユニーク制約のバリデーションを設定

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,3 +1,3 @@
 class ProductCategory < ApplicationRecord
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: true
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,5 @@
 class Store < ApplicationRecord
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: true
 
 
 end
