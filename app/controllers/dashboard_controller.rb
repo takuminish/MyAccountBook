2ea1b54a774212355b651690f77e29c58dd558f2
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-07-27 23:30:09 +0900

    棒グラフの表示

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,15 @@ class DashboardController < ApplicationController
 
     def index
 
-        @date_purchases = DatePurchase.where(date: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day)
+        @date_purchases = []
+        @week_date = []
+        7.times do |d| 
+            @week_date << (1.week.ago.beginning_of_day + d.day).strftime("%Y-%m-%d")
+            @date_purchases  << DatePurchase.find_by(date: 1.week.ago.beginning_of_day + d.day)
+        end
+        p @date_purchases
         gon.date_purchase = @date_purchases
+        gon.week = @week_date
     end
 
 end
