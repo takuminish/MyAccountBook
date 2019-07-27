8da219270146f967837079e53ab91560622ead1f
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-07-27 22:01:25 +0900

    Chart.jsのテスト表示

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
 
     def index
 
+        @date_purchases = DatePurchase.where(date: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day)
+        gon.date_purchase = @date_purchases
     end
 
 end
