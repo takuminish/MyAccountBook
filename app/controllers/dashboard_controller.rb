e51f0904c1dc214462a67f3905cf4e8432c62492
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-07-28 19:52:38 +0900

    週ごとの集計

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -16,16 +16,35 @@ class DashboardController < ApplicationController
             @year_date_purchases  << DatePurchase.where(date: (Date.today.beginning_of_year + d.month)..(Date.today.beginning_of_year.end_of_month+ d.month)).sum(:total_cost)
         end
 
+        @month_date = []
+        @month_date_purchases = []
+        4.times do |d|
+            @month_date << (Date.today.beginning_of_month.beginning_of_week + d.week).strftime("%m-%d week")
+            @month_date_purchases << DatePurchase.where(date: (Date.today.beginning_of_month.beginning_of_week + d.week)..(Date.today.beginning_of_month.end_of_week + d.week)).sum(:total_cost)
+        end
+        if ((Date.today.beginning_of_month.beginning_of_week + 5.week) < Date.today.end_of_month)
+            @month_date_purchases << DatePurchase.where(date: (Date.today.beginning_of_month.beginning_of_week + 5.week)..(Date.today.end_of_month)).sum(:total_cost)
+        end
+
+
+
+
         DatePurchase.joins({:store_purchases => {:products => :product_category}}).select("product_category_id, product_categories.name, sum(products.price) AS total_cost").group("products.product_category_id").where(date: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day).each do |p|
            @product << p.attributes
         end
+
+        @month = DatePurchase.where(date: Date.today.beginning_of_month..Date.today.end_of_month.end_of_month).group("strftime('%Y-%W weeks', date)").sum(:total_cost)
+
 
         p @date_purchases
         gon.week_date_purchase = @week_date_purchases
         gon.year_date_purchase = @year_date_purchases
         gon.year = @year_date
         gon.week = @week_date
+        gon.month_date_purchase = @month_date_purchases
+        gon.month = @month_date
         gon.product = @product
+        
     end
 
 end
