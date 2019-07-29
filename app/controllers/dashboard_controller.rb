49a95b5d5d37aaefa3960c34b49abbd924c74fd9
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-07-29 19:36:29 +0900

    scopeを用いてリファクタリング

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,37 +6,30 @@ class DashboardController < ApplicationController
         @product = []
         @week_date = []
         7.times do |d| 
-            @week_date << (Date.today.beginning_of_year + d.month).strftime("%Y-%m-%d")
-            @week_date_purchases  << DatePurchase.find_by(date: 1.week.ago.beginning_of_day + d.day)
+            @week_date << (1.week.ago.beginning_of_day + d.day).strftime("%Y-%m-%d")
+            @week_date_purchases  << DatePurchase.cost_date_by_week(d)
         end
         @year_date = []
         @year_date_purchases = []
         12.times do |d| 
             @year_date << (Date.today.beginning_of_year + d.month).strftime("%Y-%m")
-            @year_date_purchases  << DatePurchase.where(date: (Date.today.beginning_of_year + d.month)..(Date.today.beginning_of_year.end_of_month+ d.month)).sum(:total_cost)
+            @year_date_purchases  << DatePurchase.cost_month_by_year(d);
         end
 
         @month_date = []
         @month_date_purchases = []
         4.times do |d|
             @month_date << (Date.today.beginning_of_month.beginning_of_week + d.week).strftime("%m-%d week")
-            @month_date_purchases << DatePurchase.where(date: (Date.today.beginning_of_month.beginning_of_week + d.week)..(Date.today.beginning_of_month.end_of_week + d.week)).sum(:total_cost)
+            @month_date_purchases << DatePurchase.cost_week_by_month(d)
         end
         if ((Date.today.beginning_of_month.beginning_of_week + 5.week) < Date.today.end_of_month)
-            @month_date_purchases << DatePurchase.where(date: (Date.today.beginning_of_month.beginning_of_week + 5.week)..(Date.today.end_of_month)).sum(:total_cost)
+            @month_date_purchases << DatePurchase.cost_week_by_month(5)
         end
 
-
-
-
-        DatePurchase.joins({:store_purchases => {:products => :product_category}}).select("product_category_id, product_categories.name, sum(products.price) AS total_cost").group("products.product_category_id").where(date: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day).each do |p|
+        DatePurchase.cost_of_product_category_by_year(0).each do |p|
            @product << p.attributes
         end
 
-        @month = DatePurchase.where(date: Date.today.beginning_of_month..Date.today.end_of_month.end_of_month).group("strftime('%Y-%W weeks', date)").sum(:total_cost)
-
-
-        p @date_purchases
         gon.week_date_purchase = @week_date_purchases
         gon.year_date_purchase = @year_date_purchases
         gon.year = @year_date
--- app/models/date_purchase.rb
+++ app/models/date_purchase.rb
@@ -6,6 +6,21 @@ class DatePurchase < ApplicationRecord
   validates :total_cost, presence: true
   validates :date, presence: true, uniqueness: true
 
+  scope :cost_date_by_week, -> (next_date){find_by(date: 1.week.ago.beginning_of_day + next_date.day)}
+  scope :month_by_year, -> (next_month){where(date: (Date.today.beginning_of_year + next_month.month)..(Date.today.beginning_of_year.end_of_month+ next_month.month))}
+  scope :cost_month_by_year, -> (month){month_by_year(month).sum(:total_cost)}
+  scope :week_by_month, -> (next_week){where(date: (Date.today.beginning_of_month.beginning_of_week + next_week.week)..(Date.today.beginning_of_month.end_of_week + next_week.week))}
+  scope :cost_week_by_month, -> (week){week_by_month(week).sum(:total_cost)}
+  scope :join_products, ->{joins({:store_purchases => {:products => :product_category}})}
+  scope :select_product_category_cost, -> {select("product_category_id, product_categories.name, sum(products.price) AS total_cost")}
+  scope :group_by_product_category, -> {group("products.product_category_id")}
+  scope :by_week, -> (num){where(date: num.week.ago.beginning_of_week..num.week.ago.end_of_week)}
+  scope :by_month, -> (num){where(date: num.month.ago.beginning_of_month..num.week.ago.end_of_month)}
+  scope :by_year, -> (num){where(date: num.year.ago.beginning_of_year..num.week.ago.end_of_year)}
+  scope :cost_of_product_category_by_week, -> (num){join_products.select_product_category_cost.group_by_product_category.by_week(num)}
+  scope :cost_of_product_category_by_month, -> (num){join_products.select_product_category_cost.group_by_product_category.by_month(num)}
+  scope :cost_of_product_category_by_year, -> (num){join_products.select_product_category_cost.group_by_product_category.by_year(num)}
+
   def total_cost_insert
     total_cost = 0;
     store_purchases = self.store_purchases
